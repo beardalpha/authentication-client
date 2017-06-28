b72c009c9905d3041f5514038265d6bb5a3d3a27
Sohail <sohailmomin1@gmail.com>
2017-06-28 16:51:19 +0530

    adds actions to auth and unauth user

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,4 +1,5 @@
 import axios from 'axios';
+import {AUTH_USER} from './types';
 import {browserHistory} from 'react-router';
 
 const ROOT_URL = 'http://localhost:3000'; 
@@ -7,6 +8,8 @@ export function signinUser({email, password}) {
 	return function(dispatch) {
 		axios.post(`${ROOT_URL}/signup`, {email, password})
 			.then((response)=>{
+				dispatch({type: AUTH_USER});
+				localStorage.setItem('token', response.data.token);
 				browserHistory.push('/feature');
 			})
 			.catch(()=>{
